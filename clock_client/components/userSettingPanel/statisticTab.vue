3741027f498cb48ff2b565863d70bc3e27dc4c25
Ta Minh Khoi <khoitm@otani-trading.com>
2025-10-01 16:14:47 +0700

    feat:clock-client: add chart

--- clock_client/components/userSettingPanel/statisticTab.vue
+++ clock_client/components/userSettingPanel/statisticTab.vue
@@ -1,58 +1,33 @@
 <script setup lang="ts">
-import {
-	NewsIcon,
-	BrandDiscordIcon,
-	BrandTwitterIcon,
-	BrandFacebookIcon,
-	BrandRedditIcon,
-} from "vue-tabler-icons";
-import _ from "lodash";
-
-import { useMain } from "~~/stores/main";
-import { useAuthStore } from "~~/stores/auth";
-import { Control } from "~~/components/settings/types/settingsItem";
-import { fetchWithAuth } from "~~/utils/betterFetch";
-
-import ButtonControl from "~~/components/base/uiButton.vue";
-import { ButtonImportance } from "../base/types/button";
-import { AppPlatform } from "~~/platforms/platforms";
-
-import SettingsItemV2 from "~~/components/settings/settingsItemV2.vue";
 import DateRangePicker from "@/components/ui/date-picker/DateRangePicker.vue";
-
-const { userInfo, getCurrentUser } = useAuthStore();
-const runtimeConfig = useRuntimeConfig();
-const API_URL = runtimeConfig.public.API_URL;
-const isMobile = computed(
-	() => runtimeConfig.public.PLATFORM === AppPlatform.mobile,
-);
-const mainStore = useMain();
+import { BarChart } from "@/components/ui/chart-bar"
 
 const loading = ref(false);
-const updateAbleUserInfo = ["avatar_url", "name"];
-const updateUserInfo = async () => {
-	// userInfo.value
-	loading.value = true;
-	const payload = _.pick(userInfo.value, updateAbleUserInfo);
-	try {
-		const response = await fetchWithAuth(`${API_URL}/users/self/info`, {
-			method: "PATCH",
-			body: JSON.stringify(payload),
-		});
-		if (!response?.ok) throw new Error(`Fail to update user information`);
-		const data = await response.json();
-		localStorage.removeItem("Authorization");
-		localStorage.setItem("Authorization", data.token);
-		await getCurrentUser();
-	} finally {
-		loading.value = false;
-	}
-};
+
+const data = [
+  { name: "Jan", total: Math.floor(Math.random() * 2000) + 500, predicted: Math.floor(Math.random() * 2000) + 500 },
+  { name: "Feb", total: Math.floor(Math.random() * 2000) + 500, predicted: Math.floor(Math.random() * 2000) + 500 },
+  { name: "Mar", total: Math.floor(Math.random() * 2000) + 500, predicted: Math.floor(Math.random() * 2000) + 500 },
+  { name: "Apr", total: Math.floor(Math.random() * 2000) + 500, predicted: Math.floor(Math.random() * 2000) + 500 },
+  { name: "May", total: Math.floor(Math.random() * 2000) + 500, predicted: Math.floor(Math.random() * 2000) + 500 },
+  { name: "Jun", total: Math.floor(Math.random() * 2000) + 500, predicted: Math.floor(Math.random() * 2000) + 500 },
+  { name: "Jul", total: Math.floor(Math.random() * 2000) + 500, predicted: Math.floor(Math.random() * 2000) + 500 },
+]
 </script>
 
 <template>
   <div class="flex flex-col items-center">
-    <div class="inline-block text-2xl font-bold">Coming soon...</div>
 	<DateRangePicker />
+  <BarChart
+    index="name"
+    :data="data"
+    :categories="['total', 'predicted']"
+    :y-formatter="(tick, i) => {
+      return typeof tick === 'number'
+        ? `$ ${new Intl.NumberFormat('us').format(tick).toString()}`
+        : ''
+    }"
+    :rounded-corners="4"
+  />
   </div>
 </template>
