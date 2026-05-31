62b4cb14707f192ddae9ad0134c874dcdc39059c
root <root@linux4449.cloudnest.vn>
2026-05-31 03:54:40 +0000

    fix: betterme news

--- news_client/src/utils/config.ts
+++ news_client/src/utils/config.ts
@@ -2,8 +2,8 @@ const isProd = import.meta.env.PROD;
 const devServerLink = "http://server:8080";
 const devClientServerLink = "http://local.betterme.study";
 const prodServerLink = "https://api.betterme.study";
-const fetchLink = isProd ? `${prodServerLink}/api` : `${devServerLink}/api`;
+const fetchLink = isProd ? `${prodServerLink}/api/v2` : `${devServerLink}/api/v2`;
 const clientFetchLink = isProd
-	? `${prodServerLink}/api`
-	: `${devClientServerLink}/api`;
+	? `${prodServerLink}/api/v2`
+	: `${devClientServerLink}/api/v2`;
 export { fetchLink, clientFetchLink };
